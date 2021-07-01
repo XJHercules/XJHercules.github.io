bf886f369843557f0a40b912dd580252560169ea
XJHercules <smolhero6@gmail.com>
2021-07-01 16:03:51 +0000

    adding projects

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -15,71 +15,70 @@ var init = function (window) {
         window.opspark.game = {};
         var game = window.opspark.game;
         
-        ////////////////////////////////////////////////////////////
-        ///////////////// PROGRAM SETUP ////////////////////////////
-        ////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////
+        // ALL CODE GOES BELOW HERE                                   //
+        ////////////////////////////////////////////////////////////////
         
-        // TODO 1 : Declare and initialize our variables
+        // TODO 1 : Declare and initialize our variables //
         var circle;
         var circles = [];
 
-        // TODO 2 : Create a function that draws a circle 
-        function drawCircle(){
-        
-        };
 
-        // TODO 3 / 8 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        ////////////////////////////////////////////////////////////
-        ///////////////// PROGRAM LOGIC ////////////////////////////
-        ////////////////////////////////////////////////////////////
-        
-        /* 
-        This Function is called 60 times/second producing 60 frames/second.
-        In each frame, for every circle, it should redraw that circle
-        and check to see if it has drifted off the screen.         
-        */
-        function update() {
-            // TODO 4 : Update the circle's position //
-
+        // TODO 2 : Create a function that draws a circle  //
+        var drawCircle = function() {
+            // TODO 2: Draw a circle //
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
             
-            // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+        }
+        
+        // TODO 3 : Call the drawCircle function 5 times //
 
-            // TODO 9 : Iterate over the array
-           
-            
+        // TODO 7 : Create a Loop to call drawCircle 100 times
+        var counter = 0;
+        while (counter < 100) {
+            drawCircle();
+            counter++;
         }
     
-        /* 
-        This Function should check the position of a circle that is passed to the 
-        Function. If that circle drifts off the screen, this Function should move
-        it to the opposite side of the screen.
-        */
-        game.checkCirclePosition = function(circle) {
-
-            // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            }
-            
-            // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-            
-
-
-            // YOUR TODO 7 CODE ENDS HERE //////////////////////////
-        }
-        
-        /////////////////////////////////////////////////////////////
-        // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
-        /////////////////////////////////////////////////////////////
-        
         view.addChild(fps);
         app.addUpdateable(fps);
+    
+        game.checkCirclePosition = function(circle) {
+            var areaWidth = canvas.width + circle.radius;
+            var areaHeight = canvas.height + circle.radius;
+            // TODO 5 : YOUR CODE STARTS HERE //////////////////////
+            if ( circle.x > areaWidth ) {
+                circle.x = - circle.radius;
+            } 
+            else if ( circle.x < - circle.radius ) {
+                circle.x = areaWidth;
+            } 
+            if ( circle.y > areaHeight ) {
+                circle.y = - circle.radius;
+            } 
+            else if ( circle.y < - circle.radius ) {
+                circle.y = areaHeight;
+            }
+            // YOUR TODO 5 CODE ENDS HERE //////////////////////////
+        }
+    
+        function update() {
+            
+    
+            // TODO 8 : Iterate over the array
+            for (var i = 0; i < circles.length; i++) {
+                var circle = circles[i];
+                physikz.updatePosition(circle);
+                game.checkCirclePosition(circle);
+            }
+        }
+        
+        ////////////////////////////////////////////////////////////////////
+        // NO CODE BELOW HERE                                             //
+        ////////////////////////////////////////////////////////////////////
         
         game.circle = circle;
         game.circles = circles;
@@ -90,9 +89,11 @@ var init = function (window) {
     }
 };
 
+
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
 }
+
